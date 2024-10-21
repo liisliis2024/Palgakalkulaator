6b018776e8de25aa20717c9b16bfbb4631d28060
Madli Petuhhov, Mikk Sarv <madli.petuhhov89@gmail.com,  mikksrv@gmail.com>
2024-10-21 16:48:15 +0300

    calculate socialaxRate and emtInsuranceRateEmployer + tests

--- src/main/java/org/example/Palgakalkulaator.java
+++ src/main/java/org/example/Palgakalkulaator.java
@@ -4,39 +4,54 @@ public class Palgakalkulaator {
 
     public static final double INCOME_TAX_RATE = 0.2;
     public static final double ACC_PENSION_RATE = 0.02;
-    public static final double EMT_INSURANCE_RATE = 0.016;
+    public static final double EMT_INSURANCE_RATE_EMPLOYEE = 0.016;
+    public static final double SOCIAL_TAX_RATE = 0.33;
+    public static final double EMT_INSURANCE_RATE_EMPLOYER = 0.008;
 
     public static final double TAX_FREE_MIN_SALARY = 1200;
     public static final double TAX_FREE_MAX_SALARY = 2100;
 
 
-    public double netSalary(double grossSalary){
+    public double netSalary(double grossSalary) {
 
         return grossSalary - incomeTax(grossSalary)
-                 - accPension(grossSalary)
-                 - emtInsurance(grossSalary);
+                - accPension(grossSalary)
+                - emtInsuranceEmployee(grossSalary);
     }
 
-    public double incomeTax(double grossSalary){
-        double incomeTaxSum = grossSalary - accPension(grossSalary)- emtInsurance(grossSalary)- calculateIncomeTaxFreeMin(grossSalary);
-        return incomeTaxSum* INCOME_TAX_RATE;
+    public double incomeTax(double grossSalary) {
+        double incomeTaxSum = grossSalary - accPension(grossSalary) - emtInsuranceEmployee(grossSalary) - calculateIncomeTaxFreeMin(grossSalary);
+        return incomeTaxSum * INCOME_TAX_RATE;
     }
 
-    public double calculateIncomeTaxFreeMin (double grossSalary){
+    public double calculateIncomeTaxFreeMin(double grossSalary) {
         var incomeTaxMin = 654.0;
 
-        if(grossSalary >= TAX_FREE_MIN_SALARY && grossSalary < TAX_FREE_MAX_SALARY){
-           incomeTaxMin = (incomeTaxMin - incomeTaxMin / 900.0 * (grossSalary - TAX_FREE_MIN_SALARY));
-        } else if (grossSalary >= TAX_FREE_MAX_SALARY ) {
+        if (grossSalary >= TAX_FREE_MIN_SALARY && grossSalary < TAX_FREE_MAX_SALARY) {
+            incomeTaxMin = (incomeTaxMin - incomeTaxMin / 900.0 * (grossSalary - TAX_FREE_MIN_SALARY));
+        } else if (grossSalary >= TAX_FREE_MAX_SALARY) {
             incomeTaxMin = 0.0;
         }
         return incomeTaxMin;
     }
 
-    public double accPension(double grossSalary){
-            return grossSalary* ACC_PENSION_RATE;
+    public double calculateAnnualSalary(double grossSalary) {
+        return grossSalary * 12;
     }
-    public double emtInsurance(double grossSalary){
-            return grossSalary* EMT_INSURANCE_RATE;
+
+    public double accPension(double grossSalary) {
+        return grossSalary * ACC_PENSION_RATE;
+    }
+
+    public double emtInsuranceEmployee(double grossSalary) {
+        return grossSalary * EMT_INSURANCE_RATE_EMPLOYEE;
+    }
+
+    public double emtInsuranceEmployer(double grossSalary) {
+        return grossSalary * EMT_INSURANCE_RATE_EMPLOYER;
+    }
+
+    public double calculateSocialTax(double grossSalary) {
+        return grossSalary * SOCIAL_TAX_RATE;
     }
 }
--- src/test/java/PalgakalkulaatorTest.java
+++ src/test/java/PalgakalkulaatorTest.java
@@ -1,31 +1,84 @@
 import org.example.Palgakalkulaator;
 import org.junit.jupiter.api.Test;
 
+import java.text.DecimalFormat;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PalgakalkulaatorTest {
     Palgakalkulaator calc = new Palgakalkulaator();
+    DecimalFormat df = new DecimalFormat("#.##");
 
     @Test
-    void incomeTaxRate() {
+    void lowerThanTaxFreeMin() {
         var example = calc.incomeTax(1000);
-        assertEquals(62.0,example);
+        assertEquals(62, example);
+    }
+
+    @Test
+    void betweenTaxFreeMinAndMax() {
+        var example = calc.netSalary(1500);
+        assertEquals(1244, example);
+    }
+
+    @Test
+    void overTaxFreeMax() {
+        var example = calc.netSalary(2200);
+        assertEquals(1696.64, Math.round(example * 100.0) / 100.0);
     }
 
     @Test
     void accPensionRate() {
         var example = calc.accPension(1000);
-        assertEquals(20.0,example);
+        assertEquals(20, example);
     }
+
     @Test
-    void emtInsuranceRate() {
-        var example = calc.emtInsurance(1000);
-        assertEquals(16.0,example);
+    void emtInsuranceRateEmployee() {
+        var example = calc.emtInsuranceEmployee(1000);
+        assertEquals(16, example);
     }
+
+    @Test
+    void emtInsuranceRateEmployer() {
+        var example = calc.emtInsuranceEmployer(1000);
+        assertEquals(8, example);
+    }
+
     @Test
     void netSalary() {
         var example = calc.netSalary(1000);
-        assertEquals(902.0,example);
+        assertEquals(902, example);
+    }
+
+    @Test
+    void calculateIncomeTaxFreeMin() {
+        var example = calc.calculateIncomeTaxFreeMin(1000);
+        assertEquals(654.00, example);
+    }
+
+    @Test
+    void calculateIncomeTaxFreeBetweenMinAndMax() {
+        var example = calc.calculateIncomeTaxFreeMin(1689);
+        assertEquals(298.66, Math.round(example * 100.0) / 100.0);
+    }
+
+    @Test
+    void calculateIncomeTaxFreeMax() {
+        var example = calc.calculateIncomeTaxFreeMin(3000);
+        assertEquals(0, example);
+    }
+
+    @Test
+    void calculateAnnualTaxFreeMin() {
+        var example = calc.calculateAnnualSalary(900);
+        assertEquals(10800.00, Math.round(example * 100.0) / 100.0);
+    }
+
+    @Test
+    void calculateSocialTax() {
+        var example = calc.calculateSocialTax(1000);
+        assertEquals(330, example);
     }
 
 }
