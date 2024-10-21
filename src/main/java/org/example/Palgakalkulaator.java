e2ac635cb2ccbc1f4d267c846f638d17d425eb2e
Madli Petuhhov, Mikk Sarv <madli.petuhhov89@gmail.com,  mikksrv@gmail.com>
2024-10-21 16:05:11 +0300

    calculateIncomeTaxFreeMin

--- src/main/java/org/example/Palgakalkulaator.java
+++ src/main/java/org/example/Palgakalkulaator.java
@@ -6,22 +6,37 @@ public class Palgakalkulaator {
     public static final double ACC_PENSION_RATE = 0.02;
     public static final double EMT_INSURANCE_RATE = 0.016;
 
+    public static final double TAX_FREE_MIN_SALARY = 1200;
+    public static final double TAX_FREE_MAX_SALARY = 2100;
+
 
     public double netSalary(double grossSalary){
+
         return grossSalary - incomeTax(grossSalary)
                  - accPension(grossSalary)
                  - emtInsurance(grossSalary);
     }
 
     public double incomeTax(double grossSalary){
-        double incomeTaxSum = grossSalary- accPension(grossSalary)- emtInsurance(grossSalary);
+        double incomeTaxSum = grossSalary - accPension(grossSalary)- emtInsurance(grossSalary)- calculateIncomeTaxFreeMin(grossSalary);
         return incomeTaxSum* INCOME_TAX_RATE;
     }
+
+    public double calculateIncomeTaxFreeMin (double grossSalary){
+        var incomeTaxMin = 654.0;
+
+        if(grossSalary >= TAX_FREE_MIN_SALARY && grossSalary < TAX_FREE_MAX_SALARY){
+           incomeTaxMin = (incomeTaxMin - incomeTaxMin / 900.0 * (grossSalary - TAX_FREE_MIN_SALARY));
+        } else if (grossSalary >= TAX_FREE_MAX_SALARY ) {
+            incomeTaxMin = 0.0;
+        }
+        return incomeTaxMin;
+    }
+
     public double accPension(double grossSalary){
             return grossSalary* ACC_PENSION_RATE;
     }
     public double emtInsurance(double grossSalary){
             return grossSalary* EMT_INSURANCE_RATE;
     }
-
 }
--- src/test/java/PalgakalkulaatorTest.java
+++ src/test/java/PalgakalkulaatorTest.java
@@ -9,23 +9,23 @@ public class PalgakalkulaatorTest {
     @Test
     void incomeTaxRate() {
         var example = calc.incomeTax(1000);
-        assertEquals(192.80,example);
+        assertEquals(62.0,example);
     }
 
     @Test
     void accPensionRate() {
         var example = calc.accPension(1000);
-        assertEquals(20,example);
+        assertEquals(20.0,example);
     }
     @Test
     void emtInsuranceRate() {
         var example = calc.emtInsurance(1000);
-        assertEquals(16,example);
+        assertEquals(16.0,example);
     }
     @Test
     void netSalary() {
         var example = calc.netSalary(1000);
-        assertEquals(771.20,example);
+        assertEquals(902.0,example);
     }
 
 }
